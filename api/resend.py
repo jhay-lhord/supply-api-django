83b6f97aaeb097503ebd5403d2178caf1c7b1fb0
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-09-02 22:30:42 +0800

    chore: change the double qoute to single quote

--- api/resend.py
+++ api/resend.py
@@ -1,24 +1,16 @@
 import os
+
 import resend
 from dotenv import load_dotenv
 
 load_dotenv()
 
-resend.api_key = os.environ["RESEND_API_KEY"]
+resend.api_key = os.environ['RESEND_API_KEY']
 
-sender_domain_name = os.environ["DOMAIN_NAME"]
-print(sender_domain_name)
+sender_domain_name = os.environ['DOMAIN_NAME']
+
 
 def send_mail(reciever, subject, html):
-    params = {
-      "from": f"email@{sender_domain_name}",
-      "to": [reciever],
-      "subject": subject,
-      "html": html
-      }
+    params = {'from': f'email@{sender_domain_name}', 'to': [reciever], 'subject': subject, 'html': html}
     email = resend.Emails.send(params)
-    print(email)
     return email
-
-
-
