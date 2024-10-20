d496261e3e8657ec3d6edc91a4bad0d3c49da9a5
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-20 11:32:59 +0800

    feat(db): modify the max length

--- api/models.py
+++ api/models.py
@@ -70,11 +70,11 @@ class CustomUser(AbstractUser):
 
 class PurchaseRequest(models.Model):
     pr_no = models.CharField(max_length=50, primary_key=True)
-    res_center_code = models.CharField(max_length=10)
-    purpose = models.CharField(max_length=50)
-    status = models.CharField(max_length=20, default='pending')
-    requested_by = models.CharField(max_length=50)
-    approved_by = models.CharField(max_length=50)
+    res_center_code = models.CharField(max_length=50)
+    purpose = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default='pending')
+    requested_by = models.CharField(max_length=255)
+    approved_by = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
