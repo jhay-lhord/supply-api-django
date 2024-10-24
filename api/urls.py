68e119f013e2b883a63f9b92328c5bc5f718418d
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-24 21:55:17 +0800

    feat: add endpoint for updating user

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('users/', UserList.as_view()),
+    path('user/<str:pk>', UserDetail.as_view()),
     path('item/', ItemList.as_view()),
     path('item/<str:pk>', ItemDetail.as_view()),
     path('purchase-request/', PurchaseRequestList.as_view()),
