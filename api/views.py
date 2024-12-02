6814dfed3303ffc9c2c391dfcb9d10083e49ce28
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-12-02 17:37:52 +0800

    fix: adjust the days of the week to align the data.

--- api/views.py
+++ api/views.py
@@ -559,7 +559,7 @@ class DailyReportView(APIView):
             .annotate(total_abstract=Count("aoq_no"))
         )
         # Initialize Combined Data with All Days of the Week
-        day_mapping = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
+        day_mapping = {1: "Sunday", 2: "Monday", 3: "Tuesday", 4: "Wednesday", 5: "Thursday", 6: "Friday", 7: "Saturday"}
         combined_data = {day: {"day": day, "total_approved": 0, "total_quotation": 0, "total_abstract": 0} for day in day_mapping.values()}
         # Process and Combine Data
         for data, key in zip([approved, quotation, abstract], ["total_approved", "total_quotation", "total_abstract"]):
